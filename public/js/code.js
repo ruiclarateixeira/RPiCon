56f8cede5fc5fa79f3d0f7a2d5ce6057582d1ed1
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2017-11-26 22:14:52 +0000

    Stream output of process run

---
+++ public/js/code.js
@@ -0,0 +1,14 @@
+function run() {
+  $.get("/run?path=" + getCurrentFile(), function(data) {
+    var socket = new WebSocket("ws://localhost:8001");
+    socket.onopen = function(event) {
+      socket.send("RUN " + data.token);
+    };
+
+    socket.onmessage = function(event) {
+      var text = event.data;
+      text = text.replace(/\n/g, "<br>");
+      $("#termout").append(text);
+    };
+  });
+}
--- server.js
+++ server.js
@@ -1,15 +1,10 @@
-/**
- * TODO:
- * Return error instead of success for errors
- * 
- */
-
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
-var files = require("./files.js");
-var utils = require("./utils.js");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const files = require("./files.js");
+const utils = require("./utils.js");
 const opn = require("opn");
+const ws = require("nodejs-websocket");
 const { spawn } = require("child_process");
 
 app.listen(3000, function() {
@@ -24,13 +19,42 @@ app.get("/dir", files.listDirectory);
 app.post("/file", files.saveFile);
 app.get("/file", files.getFile);
 
+var scriptsToRun = {};
+var index = 0;
+
+var server = ws
+  .createServer(function(conn) {
+    conn.on("text", function(str) {
+      if (str.startsWith("RUN ")) {
+        var token = str.substring(4, str.length);
+        var path = scriptsToRun[token].path;
+
+        console.log("Running code for token " + token + ": " + path);
+        var process = spawn("python", [path]);
+
+        process.stdout.setEncoding("utf8");
+        process.stdout.on("data", function(data) {
+          conn.send(data);
+        });
+
+        process.stdout.on("end", function(data) {
+          console.log("Token " + token + ": closing connection.");
+          conn.close();
+        });
+      }
+    });
+
+    conn.on("close", function(code, reason) {});
+  })
+  .listen(8001);
+
 function run_python(req, res) {
   utils.checkParams(req, res, ["path"]);
-  var process = spawn("python", [req.query.path]);
-
-  process.stdout.on("data", function(data) {
-    res.send(data.toString());
-  });
+  scriptsToRun[index] = {
+    path: req.query.path
+  };
+  res.send({ token: index });
+  index++;
 }
 
 opn("http://localhost:3000/");
