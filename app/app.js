cdacd0ad046930b0faa8988e79e4914e7d340722
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2017-12-15 21:28:47 +0000

    Merge backend into electron

--- app/app.js
+++ app/app.js
@@ -1,7 +1,32 @@
 // {app}            Module to control application life.
 // {BrowserWindow}  Module to create native browser window.
 const { app, BrowserWindow } = require("electron");
+const express = require("express");
+const expressApp = express();
+const bodyParser = require("body-parser");
+const files = require("./files.js");
+const code = require("./code.js");
 
+/**
+ * REST and Run server
+ */
+expressApp.listen(3000, function() {
+  console.log("server running on port 3000");
+});
+
+expressApp.use(express.static("public"));
+expressApp.use(bodyParser.json());
+
+expressApp.get("/run", code.runPython);
+expressApp.get("/dir", files.listDirectory);
+expressApp.post("/file", files.saveFile);
+expressApp.get("/file", files.getFile);
+
+var server = code.createRunServer().listen(8001);
+
+/**
+ * Electron
+ */
 var mainWindow = null;
 
 // Quit when all windows are closed.
