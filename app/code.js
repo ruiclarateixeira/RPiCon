8b1bfa968c59e842cfddbeb1e5d12891961c1cc6
Rui Teixeira <rui.clarateixeira@student.manchester.ac.uk>
2017-12-16 13:43:39 +0000

    Handle paths better when running code

--- app/code.js
+++ app/code.js
@@ -1,4 +1,5 @@
 const ws = require("nodejs-websocket");
+const path = require("path");
 const { spawn } = require("child_process");
 const utils = require("./utils.js");
 
@@ -10,14 +11,19 @@ exports.createRunServer = () => {
     conn.on("text", function(str) {
       if (str.startsWith("RUN ")) {
         var token = str.substring(4, str.length);
-        var path = scriptsToRun[token].path;
+        var scriptPath = scriptsToRun[token].path;
         var env = Object.create(process.env);
 
-        if (env.PYTHONPATH == null) env.PYTHONPATH = "./pyimports";
-        else env.PYTHONPATH += ":./pyimports";
+        console.log(__dirname);
+        if (env.PYTHONPATH == null)
+          env.PYTHONPATH = __dirname + "/../pyimports";
+        else env.PYTHONPATH += ":" + __dirname + "/../pyimports";
 
-        console.log("Running code for token " + token + ": " + path);
-        var pyProcess = spawn("python", [path], { env: env });
+        console.log("Running code for token " + token + ": " + scriptPath);
+        var pyProcess = spawn("python", [scriptPath], {
+          env: env,
+          cwd: path.dirname(scriptPath)
+        });
 
         pyProcess.stdout.setEncoding("utf8");
         pyProcess.stdout.on("data", function(data) {
